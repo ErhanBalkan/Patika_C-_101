c39ff793b795a3159af619bac5e1f15dc1225964
ErhanBalkan <erhanBalkan@outlook.com.tr>
2022-12-15 13:08:36 +0300

    Hata yönetimi bölümü eklendi.

---
+++ Hata_yonetimi/Odev/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+try{
+    Console.Write("Bir sayi giriniz: ");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Girdiğiniz sayi: " + sayi);
+}
+catch (Exception ex) {
+    Console.WriteLine("Hata: " + ex.Message);
+}
+finally{
+    Console.WriteLine("İşlem tamamlandi.");
+}
+
+try{
+    int a = int.Parse(null);
+}
+catch (ArgumentNullException ex){
+    Console.WriteLine("Hata: " + ex.Message);
+    Console.WriteLine("Hata TR: Boş değer girdiniz!");
+}
+
+
+try{
+    int b = int.Parse("test");
+}
+catch (FormatException ex){
+    Console.WriteLine("Hata: " + ex.Message);
+    Console.WriteLine("Hata TR: Yanlis veri tipi");
+}
+
+
+try{
+    int c = int.Parse("20000000000");
+}
+catch (OverflowException ex){
+    Console.WriteLine("Hata: " + ex.Message);
+    Console.WriteLine("Hata TR: Değer disina cikildi.");
+}
