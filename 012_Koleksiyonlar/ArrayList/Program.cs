362435b03755de2e631550e1effe16de709cc815
ErhanBalkan <erhanBalkan@outlook.com.tr>
2022-12-23 19:09:41 +0300

    ArrayList konusu eklendi.

---
+++ 012_Koleksiyonlar/ArrayList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+ArrayList liste = new ArrayList();
+liste.Add("Erhan");
+liste.Add(2);
+liste.Add(true);
+liste.Add('A');
+
+System.Console.WriteLine(liste[1]);
+foreach (var item in liste)
+{
+    System.Console.WriteLine(item);
+}
+
+// Add range
+string[] renkler = {"Kırmızı", "Sarı", "Yeşil"};
+List<int> sayilar = new List<int>(){1,8,3,7,9,92,5};
+
+liste.AddRange(renkler);
+liste.AddRange(sayilar);
+
+// listeye liste ekledik
+
