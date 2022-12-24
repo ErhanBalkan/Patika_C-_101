e32411a4076189ae36f3b31f42c7f75e6c74bfe9
ErhanBalkan <erhanBalkan@outlook.com.tr>
2022-12-24 16:14:02 +0300

    Constructor konusu kodları eklendi.

--- 013_Sinifflar/Sinif_kavrami/Calisan.cs
+++ 013_Sinifflar/Sinif_kavrami/Calisan.cs
@@ -5,6 +5,19 @@ class Calisan
     public int No;
     public string Departman;
 
+    public Calisan(string ad, string Soyad, int No, string Departman)
+    {
+        this.Ad = ad;
+        this.Soyad = Soyad;
+        this.No = No;
+        this.Departman = Departman;
+    }
+    public Calisan(string ad, string soyad)
+    {
+        this.Ad = ad;
+        this.Soyad = soyad;
+    }
+    public Calisan(){}
     public void CalisanBilgileri(){
         System.Console.WriteLine($"Çalışanın Adı: {Ad}");
         System.Console.WriteLine($"Çalışanın Soyadı: {Soyad}");
--- 013_Sinifflar/Sinif_kavrami/Program.cs
+++ 013_Sinifflar/Sinif_kavrami/Program.cs
@@ -5,3 +5,19 @@ calisan1.No = 24506;
 calisan1.Departman = "İskelet Robot Operatörü";
 
 calisan1.CalisanBilgileri();
+
+Calisan calisan2 = new Calisan{
+    Ad = "Atakan",
+    Soyad = "Aydın",
+    No = 24500,
+    Departman = "İskelet Robot Operatörü"
+};
+
+calisan2.CalisanBilgileri();
+
+Calisan calisan3 = new Calisan{
+    Ad = "Adnan",
+    Soyad = "Namlucuoğlu",
+};
+
+calisan3.CalisanBilgileri();
